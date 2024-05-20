0f4eb8ebf5857f048fd989dd06dc854e32746cf6
DimaV <dmytrovoronov@gmail.com>
2024-05-19 23:58:00 -0400

    [orders] added mail sender

--- internal/mail/email_sender.go
+++ internal/mail/email_sender.go
@@ -1,10 +1,40 @@
 package mail
 
-import "fmt"
+import (
+	"fmt"
+	"orders/internal/config"
+	"orders/internal/models"
+	"strconv"
+
+	"gopkg.in/gomail.v2"
+)
 
 type EmailSender struct {
+	From string
 }
 
-func (e *EmailSender) SendEmail(userID int, mailID int) {
-	fmt.Printf("EmailSender: Sending email for user %d; Mail ID: %d", userID, mailID)
+func NewEmailSender() *EmailSender {
+	from := "mailtrap@demomailtrap.com"
+	return &EmailSender{
+		From: from,
+	}
+}
+
+func (e *EmailSender) SendEmail(o *models.Order) error {
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", "mailtrap@demomailtrap.com")
+	msg.SetHeader("To", o.CustomerInfo.Email)
+	msg.SetHeader("Subject", fmt.Sprintf("Order #%d", o.ID))
+	msg.SetBody("text/html", "Thank you for your order")
+
+	port, err := strconv.Atoi(config.Env("MAIL_PORT"))
+	if err != nil {
+		return err
+	}
+	n := gomail.NewDialer(config.Env("MAIL_HOST"), port, config.Env("MAIL_USERNAME"), config.Env("MAIL_PASSWORD"))
+
+	if err := n.DialAndSend(msg); err != nil {
+		return err
+	}
+	return nil
 }
